65064b4ae58c5c898f910409c0fdd24446ec33b1
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2018-05-14 16:26:03 -0600

    Add icons

--- desktop/src/science/skywhale/drop/desktop/DesktopLauncher.java
+++ desktop/src/science/skywhale/drop/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package science.skywhale.drop.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import science.skywhale.drop.Drop;
@@ -12,6 +13,10 @@ public class DesktopLauncher
 		config.title = "Drop";
 		config.width = 800;
 		config.height = 480;
+		config.foregroundFPS = config.backgroundFPS = 120;
+		config.addIcon("bucket.png", Files.FileType.Internal);
+		config.addIcon("bucketSmall.png", Files.FileType.Internal);
+		config.addIcon("bucketTiny.png", Files.FileType.Internal);
 		new LwjglApplication(new Drop(), config);
 	}
 }
