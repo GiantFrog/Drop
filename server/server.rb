3183ddf2e8435f236023e07d8180a82685c2430b
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2018-06-14 13:08:08 -0600

    Improve server stability

--- server/server.rb
+++ server/server.rb
@@ -17,38 +17,48 @@ end
 file.close()
 
 loop do		#accept new entries
-  client = server.accept	#wait here until somebody connects
-  new_entry_parts = client.gets.chomp.split ':'
-	puts new_entry_parts[0] + ' has connected!'
-	new_entry = LeaderboardEntry.new new_entry_parts[0], new_entry_parts[1].to_i
+	begin
+		client = server.accept	#wait here until somebody connects
+		new_entry_parts = client.gets.chomp.split ':'
+		puts new_entry_parts[0] + ' has connected!'
+		new_entry = LeaderboardEntry.new new_entry_parts[0], new_entry_parts[1].to_i
 
-	added = false		#add them to the leaderboard
-	leaderboard.each_with_index do |entry, index|
-		if new_entry.score > entry.score
-			leaderboard.insert index, new_entry
-			added = true
-			break
+		added = false		#add them to the leaderboard
+		leaderboard.each_with_index do |entry, index|
+			if new_entry.score > entry.score
+				leaderboard.insert index, new_entry
+				added = true
+				break
+			end
+		end
+		unless added		#put it at the end
+			leaderboard << new_entry
+		end
+
+		#save the file
+		file = File.open('leaderboard.txt', 'w+')
+		leaderboard.each do |entry|
+			file.puts(entry.name + ': ' + entry.score.to_s)
+		end
+		file.print '&'
+		file.close
+
+		#give the client the file
+		board_string = IO.read('leaderboard.txt')
+		board_string.each_line do |line|
+			client.puts line
+		end
+
+		client.gets		#wait for client to do their thing
+		puts 'saved and sent file!'
+		client.close
+
+	rescue
+		#try to close the connection if things go south, then start anew
+		begin
+			client.close
+		rescue
+			#connection isn't open. okay.
 		end
 	end
-	unless added		#put it at the end
-		leaderboard << new_entry
-	end
-
-	#save the file
-	file = File.open('leaderboard.txt', 'w+')
-	leaderboard.each do |entry|
-		file.puts(entry.name + ': ' + entry.score.to_s)
-	end
-	file.print '&'
-	file.close
-
-	#give the client the file
-	board_string = IO.read('leaderboard.txt')
-	board_string.each_line do |line|
-		client.puts line
-	end
-
-	client.gets		#wait for client to do their thing
-	puts 'saved and sent file!'
-	client.close
 end
